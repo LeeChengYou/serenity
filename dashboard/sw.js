a7ecf5aee0510643821cd0e4d0f6572570183799
LeeChengYou <901126jeff901126@gmail.com>
2026-07-18 23:48:11 +0800

    chore: bump cache version for region filter feature

--- dashboard/sw.js
+++ dashboard/sw.js
@@ -1,6 +1,6 @@
 // Serenity Signal Service Worker
 // 版本字串：升版時更改此值以觸發 activate 清除舊快取
-const CACHE_VERSION = 'serenity-v10-tw2';
+const CACHE_VERSION = 'serenity-v11-region';
 
 // 安裝時預快取的靜態資產
 const STATIC_ASSETS = [
